eefcd4c87e4d838828cd8bc2c6dfd4a1753820d9
Max Wofford <max@maxwofford.com>
2025-05-22 02:14:13 -0400

    Add neighborhood model associations

--- app/models/neighborhood/app.rb
+++ app/models/neighborhood/app.rb
@@ -6,4 +6,14 @@ class Neighborhood::App < ApplicationRecord
   has_table_sync base: "appnsN4MzbnfMY0ai",
                  table: "tbls2fHyQYCtCbYbl",
                  pat: ENV["NEIGHBORHOOD_AIRTABLE_PAT"]
+
+  def posts
+    return [] unless airtable_fields["app"]&.first
+    Neighborhood::Post.where(airtable_id: airtable_fields["app"])
+  end
+
+  def projects
+    return [] unless airtable_fields["hackatimeProjects"]&.any?
+    Neighborhood::Project.where(airtable_id: airtable_fields["hackatimeProjects"])
+  end
 end
--- app/models/neighborhood/post.rb
+++ app/models/neighborhood/post.rb
@@ -6,4 +6,9 @@ class Neighborhood::Post < ApplicationRecord
   has_table_sync base: "appnsN4MzbnfMY0ai",
                  table: "tbl0iKxglbySiEbB4",
                  pat: ENV["NEIGHBORHOOD_AIRTABLE_PAT"]
+
+  def app
+    return nil unless airtable_fields["app"]&.first
+    Neighborhood::App.find_by(airtable_id: airtable_fields["app"].first)
+  end
 end
