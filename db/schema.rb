6f686e401d7c744e3f674a69d97deb0d371c993b
Max Wofford <max@maxwofford.com>
2025-06-16 15:26:12 -0400

    Add index for ahoy jobs on referral domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_16_191832) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_16_192214) do
   create_schema "pganalyze"
 
   # These are extensions that must be enabled in order to support this database
@@ -57,6 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_191832) do
     t.string "platform"
     t.datetime "started_at"
     t.index ["started_at"], name: "index_ahoy_visits_on_started_at"
+    t.index ["started_at"], name: "index_ahoy_visits_started_at_with_referring_domain", where: "(referring_domain IS NOT NULL)"
     t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
     t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
     t.index ["visitor_token", "started_at"], name: "index_ahoy_visits_on_visitor_token_and_started_at"
