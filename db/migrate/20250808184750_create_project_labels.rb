f382dbad2d2751416779c50c3aa1827c0934fa5c
Parth <215315028+nystar1@users.noreply.github.com>
2025-08-09 19:21:53 +0530

    feat: make create project labels infallible

--- db/migrate/20250808184750_create_project_labels.rb
+++ db/migrate/20250808184750_create_project_labels.rb
@@ -1,14 +1,15 @@
 class CreateProjectLabels < ActiveRecord::Migration[8.0]
   # where was ts before?
+  # precedent for future to make migration infallible
   def change
-    create_table :project_labels do |t|
+    create_table :project_labels, if_not_exists: true do |t|
       t.string :user_id, null: false
       t.string :project_key, null: false
       t.string :label, null: false
       t.timestamps
     end
 
-  add_index :project_labels, [ :user_id, :project_key ], unique: true
-  add_index :project_labels, :user_id
+    add_index :project_labels, [ :user_id, :project_key ], unique: true, if_not_exists: true
+    add_index :project_labels, :user_id, if_not_exists: true
   end
 end
