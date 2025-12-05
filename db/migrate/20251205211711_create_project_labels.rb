898480261958415ed7f9c898865a942495a4595a
Echo <github@3kh0.net>
2025-12-05 16:19:00 -0500

    fix broken project labels table

---
+++ db/migrate/20251205211711_create_project_labels.rb
@@ -0,0 +1,13 @@
+class CreateProjectLabels < ActiveRecord::Migration[8.1]
+  def change
+    create_table :project_labels do |t|
+      t.string :user_id
+      t.string :project_key
+      t.string :label
+
+      t.timestamps
+    end
+    add_index :project_labels, :user_id
+    add_index :project_labels, [:user_id, :project_key], unique: true
+  end
+end
