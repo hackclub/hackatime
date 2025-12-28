874603ee0f65108e08b7d31afeab1bebcd0ab5de
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-28 16:22:59 -0500

    add admin key revocation endpoint

---
+++ app/controllers/api/internal/revocations_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module Internal
+    class RevocationsController < ApplicationController
+      def create
+        token = params[:token]
+
+        return head 400 unless token.present?
+
+        admin_api_key = AdminApiKey.active.find_by(token:)
+
+        return render json: { success: false } unless admin_api_key.present?
+
+        admin_api_key.revoke!
+
+        user = admin_api_key.user
+
+        render json: {
+          success: true,
+          owner_email: user.email_addresses.first&.email
+        }.compact_blank
+      end
+
+      private def authenticate!
+        res = authenticate_with_http_token do |token, _|
+          ActiveSupport::SecurityUtils.secure_compare(token, ENV["HKA_REVOCATION_KEY"])
+        end
+        unless res
+          redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ", allow_other_host: true
+        end
+      end
+    end
+  end
+end
