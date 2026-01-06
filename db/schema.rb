fedce34f58c628af10f090d38d4a8ca8dcd41c6f
Echo <github@3kh0.net>
2026-01-06 09:42:20 -0500

    allow repo mapping on projects without a repo url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_05_230132) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_06_143925) do
   create_schema "pganalyze"
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
@@ -387,7 +387,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_05_230132) do
     t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.string "project_name", null: false
-    t.string "repo_url", null: false
+    t.string "repo_url"
     t.bigint "repository_id"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
