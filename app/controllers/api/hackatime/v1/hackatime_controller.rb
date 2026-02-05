725acc6770ae5811b36a1edb2c14784c870802ec
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-05 11:31:37 +0000

    Queue Posthog *after* adding to DB

--- app/controllers/api/hackatime/v1/hackatime_controller.rb
+++ app/controllers/api/hackatime/v1/hackatime_controller.rb
@@ -249,8 +249,6 @@ class Api::Hackatime::V1::HackatimeController < ApplicationController
   end
 
   def handle_heartbeat(heartbeat_array)
-    PosthogService.capture_once_per_day(@user, "heartbeat_sent", { heartbeat_count: heartbeat_array.size })
-
     results = []
     heartbeat_array.each do |heartbeat|
       source_type = :direct_entry
@@ -289,6 +287,8 @@ class Api::Hackatime::V1::HackatimeController < ApplicationController
       Rails.logger.error("Error creating heartbeat: #{e.class.name} #{e.message}")
       results << [ { error: e.message, type: e.class.name }, 422 ]
     end
+
+    PosthogService.capture_once_per_day(@user, "heartbeat_sent", { heartbeat_count: heartbeat_array.size })
     results
   end
 
