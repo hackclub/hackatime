ba9ecd5dea698575a3c703779fbd1686b01c98f3
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-09 11:19:58 +0000

    bin/rubocop -A

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ class AdminLevelConstraint
 end
 
 Rails.application.routes.draw do
-
   # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
   constraints(host: "127.0.0.1") do
     get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
