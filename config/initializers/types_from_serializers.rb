bb85a2b41717ce44c9ff9c53c6b3cd87d974beee
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-10 13:01:39 +0000

    Fix TFS initializer in production

--- config/initializers/types_from_serializers.rb
+++ config/initializers/types_from_serializers.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-TypesFromSerializers.config do |config|
-  config.output_dir = Rails.root.join("app/javascript/types/serializers")
-  config.custom_types_dir = Rails.root.join("app/javascript/types")
-  config.transform_keys = ->(key) { key.to_s }
+if Rails.env.development? then
+  TypesFromSerializers.config do |config|
+    config.output_dir = Rails.root.join("app/javascript/types/serializers")
+    config.custom_types_dir = Rails.root.join("app/javascript/types")
+    config.transform_keys = ->(key) { key.to_s }
+  end
 end
