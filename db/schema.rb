a37423d8213000fac5c099ec5022091425ed8364
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-15 09:42:33 +0000

    Uh-oh, re-add pganalyze

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_15_094000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
+  create_schema "pganalyze"
 
   create_table "admin_api_keys", force: :cascade do |t|
     t.datetime "created_at", null: false
