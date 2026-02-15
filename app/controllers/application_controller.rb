0e6c04d6b0b6fabecf4096a22aa467fa19478897
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-15 09:41:19 +0000

    Remove public_activity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :initialize_cache_counters
   before_action :try_rack_mini_profiler_enable
   before_action :track_request
-  before_action :set_public_activity
   before_action :enforce_lockout
 
   around_action :switch_time_zone, if: :current_user
@@ -19,11 +18,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :user_signed_in?, :active_users_graph_data
 
   private
-
-  def set_public_activity
-    return unless Flipper.enabled?(:public_activity_log, current_user)
-    @activities = PublicActivity::Activity.limit(25).order(created_at: :desc).includes(:owner, :trackable)
-  end
 
   def sentry_context
     Sentry.set_user(
