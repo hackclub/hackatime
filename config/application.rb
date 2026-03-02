a38af22b521ec103850434d7dcacbc9879ebc33d
Mahad Kalam <mahadkalam1234@gmail.com>
2026-03-02 20:05:10 +0000

    Remove maintenance mode middleware, controller, and page

--- config/application.rb
+++ config/application.rb
@@ -56,10 +56,6 @@ module Harbor
       expire_after: 14.days,
       secure: Rails.env.production?,
       httponly: true
-
-    require_relative "../lib/maintenance_mode_middleware"
-    config.middleware.insert_before 0, MaintenanceModeMiddleware
-
     config.middleware.use Rack::Attack
     config.exceptions_app = routes
   end
