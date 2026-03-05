91654cb5312af86c23f40b7a57debfa3a1216158
Mat Manna <91392083+matmanna@users.noreply.github.com>
2026-03-05 16:08:44 +0000

    fix: add other motivation features

--- app/javascript/pages/Home/signedOut/FeaturesGrid.svelte
+++ app/javascript/pages/Home/signedOut/FeaturesGrid.svelte
@@ -26,9 +26,9 @@
         <path d="M7.151 21.75a2.999 2.999 0 0 0 2.599 1.5h7.5a3 3 0 0 0 3-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 0 1-4.5 4.5H7.151Z" />`,
     },
     {
-      title: "Leaderboards (coming soon)",
+      title: "Built-in Motivation",
       description:
-        "Create private leaderboards for your team or hackathon. Compete on consistency.",
+        "Leaderboards, goals, streaks, and email summaries create competetion and consistency.",
       icon: `<path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />`,
     },
     {
