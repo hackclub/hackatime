c3ab171356f7184ba217dc05b46ed20bc57763d1
Freddie Yershon <143739931+hippogriff101@users.noreply.github.com>
2026-03-07 21:13:12 +0000

    Fix description casing for HackaStreak extension

--- app/javascript/pages/Extensions/Index.svelte
+++ app/javascript/pages/Extensions/Index.svelte
@@ -18,7 +18,7 @@
       name: "HackaStreak",
       source: "https://github.com/hippogriff101/HackaStreak",
       description:
-        "Check a user's Hackatime Streak through Slack!",
+        "Check a user's Hackatime streak in Slack.",
       install: "https://hackclub.enterprise.slack.com/archives/C0AJLGZ73NE",
     },
   ];
