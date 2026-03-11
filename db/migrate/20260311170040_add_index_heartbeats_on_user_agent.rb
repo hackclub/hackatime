1d6947903b7652bc5cb1548b9694176a4d9fec19
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-03-11 17:18:46 +0000

    Revert "Migrate Lapse hbs + concurrent index on heartbeats' user_agent (#1056)" (#1057)
    This reverts commit cda06a30be232582a7946f0dcd483299145a97e8.

--- db/migrate/20260311170040_add_index_heartbeats_on_user_agent.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexHeartbeatsOnUserAgent < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :heartbeats, :user_agent, algorithm: :concurrently
-  end
-end
