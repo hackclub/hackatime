d38b7f71f5c86e0ea9d085cd17b970bef056d952
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-03-15 20:35:48 +0000

    Mailer previews + partialify the weekly summary (#1076)
    * Mailer previews + partialify
    
    * oops

--- test/mailers/previews/weekly_summary_mailer_preview.rb
+++ test/mailers/previews/weekly_summary_mailer_preview.rb
@@ -1,7 +1,28 @@
 class WeeklySummaryMailerPreview < ActionMailer::Preview
   def weekly_summary
-    user = User.first || User.new(username: "preview_user", timezone: "UTC")
+    user = User.joins(:heartbeats).distinct.first || User.first
     ends_at = Time.current.beginning_of_week
+    starts_at = ends_at - 7.days
+
+    if user&.heartbeats&.where(time: starts_at.to_f...ends_at.to_f)&.none?
+      latest = user&.heartbeats&.order(time: :desc)&.first
+      if latest
+        ends_at = Time.at(latest.time).end_of_week + 1.day
+        starts_at = ends_at - 7.days
+      end
+    end
+
+    WeeklySummaryMailer.weekly_summary(
+      user,
+      recipient_email: "user@example.com",
+      starts_at: starts_at,
+      ends_at: ends_at
+    )
+  end
+
+  def weekly_summary_empty
+    user = User.first || User.new(username: "preview_user", timezone: "UTC")
+    ends_at = 1.year.from_now.beginning_of_week
     starts_at = ends_at - 7.days
 
     WeeklySummaryMailer.weekly_summary(
