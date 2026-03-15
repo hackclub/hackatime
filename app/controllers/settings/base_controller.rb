d8b0e80cb7a4a8798bb4bbc9a88394e4ddec6dd7
Mat Manna <matmanna@outlook.com>
2026-03-14 23:24:10 -0400

    feat: add hacka-box to badges settings

--- app/controllers/settings/base_controller.rb
+++ app/controllers/settings/base_controller.rb
@@ -187,7 +187,9 @@ class Settings::BaseController < InertiaController
         markscribe_reference_url: "https://github.com/taciturnaxolotl/markscribe#your-wakatime-languages-formated-as-a-bar",
         markscribe_preview_image_url: "https://cdn.fluff.pw/slackcdn/524e293aa09bc5f9115c0c29c18fb4bc.png",
         heatmap_badge_url: "https://heatmap.shymike.dev/?id=#{@user.id}&timezone=#{@user.timezone}",
-        heatmap_config_url: "https://hackatime-heatmap.shymike.dev/?id=#{@user.id}&timezone=#{@user.timezone}"
+        heatmap_config_url: "https://hackatime-heatmap.shymike.dev/?id=#{@user.id}&timezone=#{@user.timezone}",
+        hackabox_repo_url: "https://github.com/quackclub/hacka-box",
+        hackabox_preview_image_url: "https://user-cdn.hackclub-assets.com/019cef81-366a-7543-ad7c-21b738310f23/image.png",
       },
       config_file: {
         content: generated_wakatime_config(@latest_api_key_token),
