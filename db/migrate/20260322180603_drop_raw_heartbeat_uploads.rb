d68e9553d8801b87d494c921965664927a89fb7c
Mahad Kalam <mahadkalam1234@gmail.com>
2026-03-22 18:08:46 +0000

    Make it friendly

--- db/migrate/20260322180603_drop_raw_heartbeat_uploads.rb
+++ db/migrate/20260322180603_drop_raw_heartbeat_uploads.rb
@@ -1,9 +1,15 @@
 class DropRawHeartbeatUploads < ActiveRecord::Migration[8.1]
   def change
-    remove_foreign_key :heartbeats, :raw_heartbeat_uploads
-    remove_index :heartbeats, :raw_heartbeat_upload_id
-    remove_column :heartbeats, :raw_heartbeat_upload_id, :bigint
-    drop_table :raw_heartbeat_uploads do |t|
+    if foreign_key_exists?(:heartbeats, :raw_heartbeat_uploads)
+      remove_foreign_key :heartbeats, :raw_heartbeat_uploads
+    end
+    if index_exists?(:heartbeats, :raw_heartbeat_upload_id)
+      remove_index :heartbeats, :raw_heartbeat_upload_id
+    end
+    if column_exists?(:heartbeats, :raw_heartbeat_upload_id)
+      remove_column :heartbeats, :raw_heartbeat_upload_id, :bigint
+    end
+    drop_table :raw_heartbeat_uploads, if_exists: true do |t|
       t.text :body
       t.bigint :user_id
       t.datetime :created_at, null: false
