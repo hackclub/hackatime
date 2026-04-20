e296377b41704e7616480bffc41f0c5273c5b7da
Mahad Kalam <mahadkalam1234@gmail.com>
2026-04-20 17:39:50 +0100

    TEMP: Make HSJ not load all users...

--- app/jobs/cache/home_stats_job.rb
+++ app/jobs/cache/home_stats_job.rb
@@ -4,10 +4,14 @@ class Cache::HomeStatsJob < Cache::ActivityJob
   private
 
   def calculate
-    seconds_by_user = Heartbeat.group(:user_id).duration_seconds
+    # seconds_by_user = Heartbeat.group(:user_id).duration_seconds
+    # {
+    #   users_tracked: seconds_by_user.size,
+    #   seconds_tracked: seconds_by_user.values.sum
+    # }
     {
-      users_tracked: seconds_by_user.size,
-      seconds_tracked: seconds_by_user.values.sum
+      users_tracked: 23_000,
+      seconds_tracked: 3_600_000_000 # 1 million hours
     }
   end
 end
