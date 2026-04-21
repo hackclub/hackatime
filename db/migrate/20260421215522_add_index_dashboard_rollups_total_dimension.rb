9e300bdb89f2c124eec20bc816c1905e654913d0
Mahad Kalam <mahadkalam1234@gmail.com>
2026-04-21 22:23:05 +0100

    Add if_not_exists to partial index migration

--- db/migrate/20260421215522_add_index_dashboard_rollups_total_dimension.rb
+++ db/migrate/20260421215522_add_index_dashboard_rollups_total_dimension.rb
@@ -2,6 +2,7 @@ class AddIndexDashboardRollupsTotalDimension < ActiveRecord::Migration[8.1]
   def change
     add_index :dashboard_rollups, :dimension,
       name: "index_dashboard_rollups_on_dimension_total",
-      where: "dimension = 'total' AND total_seconds > 0"
+      where: "dimension = 'total' AND total_seconds > 0",
+      if_not_exists: true
   end
 end
