7190c416410e30f04bde0e2232e3066e29b9c21b
Mahad Kalam <mahadkalam1234@gmail.com>
2026-04-22 09:42:08 +0100

    Make `Cache::HeartbeatCountsJob` faster

--- app/jobs/cache/heartbeat_counts_job.rb
+++ app/jobs/cache/heartbeat_counts_job.rb
@@ -2,23 +2,20 @@ class Cache::HeartbeatCountsJob < Cache::ActivityJob
   queue_as :latency_10s
 
   def expires_in
-    5.hours
+    1.hour
   end
 
   private
 
   def calculate
+    recent_count, recent_imported_count = Heartbeat.recent.pluck(
+      Arel.sql("COUNT(*)"),
+      Arel.sql("COUNT(*) FILTER (WHERE source_type != 0)")
+    ).first
+
     {
-      recent_count: recent_count,
-      recent_imported_count: recent_imported_count
+      recent_count:,
+      recent_imported_count:
     }
-  end
-
-  def recent_count
-    Heartbeat.recent.count
-  end
-
-  def recent_imported_count
-    Heartbeat.recent.where.not(source_type: :direct_entry).count
   end
 end
