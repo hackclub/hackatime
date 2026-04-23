da89b8dfc1d1f944c144dfed5b072edff784e7ed
Mahad Kalam <mahadkalam1234@gmail.com>
2026-04-23 18:44:09 +0100

    oops

--- db/migrate/20260423172811_add_heartbeats_user_project_time_covering_index.rb
+++ db/migrate/20260423172811_add_heartbeats_user_project_time_covering_index.rb
@@ -2,8 +2,6 @@ class AddHeartbeatsUserProjectTimeCoveringIndex < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
   def change
-    remove_index :heartbeats, name: "idx_heartbeats_user_project_time_covering", algorithm: :concurrently, if_exists: true
-
     add_index :heartbeats, [:user_id, :project, :time],
       name: "idx_heartbeats_user_project_time_covering",
       where: "deleted_at IS NULL",
