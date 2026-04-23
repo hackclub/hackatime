704ccd1b1958e4a92d49feaa184c1f0a1570f7af
Mahad Kalam <mahadkalam1234@gmail.com>
2026-04-23 20:15:36 +0100

    bin/rubocop -A

--- db/migrate/20260423172811_add_heartbeats_user_project_time_covering_index.rb
+++ db/migrate/20260423172811_add_heartbeats_user_project_time_covering_index.rb
@@ -2,10 +2,10 @@ class AddHeartbeatsUserProjectTimeCoveringIndex < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
   def up
-    add_index :heartbeats, [:user_id, :project, :time],
+    add_index :heartbeats, [ :user_id, :project, :time ],
       name: "idx_heartbeats_user_project_time_covering",
       where: "deleted_at IS NULL",
-      include: [:category],
+      include: [ :category ],
       algorithm: :concurrently
   end
 
--- db/migrate/20260423174942_add_heartbeats_leaderboard_and_time_user_indexes.rb
+++ db/migrate/20260423174942_add_heartbeats_leaderboard_and_time_user_indexes.rb
@@ -2,16 +2,16 @@ class AddHeartbeatsLeaderboardAndTimeUserIndexes < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
   def change
-    add_index :heartbeats, [:user_id, :category, :time],
+    add_index :heartbeats, [ :user_id, :category, :time ],
       name: "idx_heartbeats_user_category_time_incl_editor",
       where: "deleted_at IS NULL",
-      include: [:editor],
+      include: [ :editor ],
       algorithm: :concurrently
 
     remove_index :heartbeats, name: "idx_heartbeats_user_category_time",
       algorithm: :concurrently, if_exists: true
 
-    add_index :heartbeats, [:time, :user_id],
+    add_index :heartbeats, [ :time, :user_id ],
       name: "idx_heartbeats_time_user_active",
       where: "deleted_at IS NULL",
       algorithm: :concurrently
