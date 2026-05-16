ca97b2f38375dde7534c9fac447b7b237b67521f
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-05-16 09:38:31 +0100

    Update dependencies + masks trust levels in OAuth2 API (#1312)
    * Update dependencies
    
    * Fix trust level exposure

--- app/controllers/api/v1/authenticated/me_controller.rb
+++ app/controllers/api/v1/authenticated/me_controller.rb
@@ -5,14 +5,19 @@ module Api
         skip_before_action :ensure_no_ban, only: :index
 
         def index
+          exposed_level = current_user.trust_level
+          unless doorkeeper_token&.application&.verified?
+            exposed_level = "blue" if exposed_level == "yellow"
+          end
+
           render json: {
             id: current_user.id,
             emails: current_user.email_addresses&.map(&:email)|| [],
             slack_id: current_user.slack_uid,
             github_username: current_user.github_username,
             trust_factor: {
-              trust_level: current_user.trust_level,
-              trust_value: User.trust_levels[current_user.trust_level]
+              trust_level: exposed_level,
+              trust_value: User.trust_levels[exposed_level]
             }
           }
         end
