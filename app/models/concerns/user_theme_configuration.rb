5a816c090cf9667d1efb6245d15b760086adb743
Mahad Kalam <mahadkalam1234@gmail.com>
2026-05-23 00:48:01 +0100

    Use YAML.safe_load_file for theme config
    YAML.load_file deserializes arbitrary YAML tags; safe_load_file restricts to
    safe types. config/themes.yml is pure data and doesn't need custom classes.

--- app/models/concerns/user_theme_configuration.rb
+++ app/models/concerns/user_theme_configuration.rb
@@ -2,7 +2,7 @@ module UserThemeConfiguration
   extend ActiveSupport::Concern
 
   DEFAULT_THEME = "rose".freeze
-  THEME_OPTIONS = YAML.load_file(Rails.root.join("config/themes.yml"))
+  THEME_OPTIONS = YAML.safe_load_file(Rails.root.join("config/themes.yml"))
                       .map { |t| t.deep_symbolize_keys.tap { |h| h[:preview] = h[:preview].transform_keys(&:to_sym) } }
                       .freeze
   THEME_OPTION_BY_VALUE = THEME_OPTIONS.index_by { |theme| theme[:value] }.freeze
