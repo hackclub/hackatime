229fd6078fb48e4c005ccf6864aa5fb1f452b9d6
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-06-06 23:14:52 +0100

    [OnboardingMailer] Update check-in details (#1411)

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -11,7 +11,7 @@ class OnboardingMailer < ApplicationMailer
 
   def check_in(user, recipient_email:)
     @user = user
-    from_email = ENV.fetch("ONBOARDING_CHECK_IN_FROM_EMAIL", "Mahad from Hackatime <mahad@hackclub.com>")
+    from_email = ENV.fetch("ONBOARDING_CHECK_IN_FROM_EMAIL", "Mahad Kalam <mahad@hackclub.com>")
     reply_to = "hackatime@hackclub.com"
 
     mail(
@@ -19,7 +19,7 @@ class OnboardingMailer < ApplicationMailer
       from: from_email,
       cc: from_email,
       reply_to:,
-      subject: "How are you finding Hackatime?"
+      subject: "How're you finding Hackatime?"
     )
   end
 end
