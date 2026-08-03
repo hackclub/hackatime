ecb9c1f8dc20dcc11f4cd7b1ebe3964fea2dc2e4
Mahad Kalam <55807755+skyfallwastaken@users.noreply.github.com>
2026-08-03 16:40:57 +0100

    Remove obsolete Solid Queue tooling (#1509)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :good_job
-  # config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # ActiveJob cache refreshes can run inline during requests and drown out page
   # logs. Keep failures visible through Rails.logger calls in job code, but hide
