e5240d3c4eec59772cae499e921f2b375a4e9911
Mahad Kalam <mahadkalam1234@gmail.com>
2026-08-20 12:18:27 +0000

    Support PNG documentation OG logos

--- blume.config.ts
+++ blume.config.ts
@@ -151,7 +151,7 @@ export default defineConfig({
   seo: {
     og: {
       enabled: true,
-      logo: "/images/hackatime-icon.svg",
+      logo: "/images/new-icon-rounded.png",
       palette: {
         accent: defaultTheme.preview.primary,
         background: defaultTheme.preview.darker,
