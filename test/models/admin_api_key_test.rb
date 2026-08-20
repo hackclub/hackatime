60837d5c8f34ac28bce48a4bdaadabc4d522a15d
Mahad Kalam <mahadkalam1234@gmail.com>
2026-08-20 20:32:54 +0100

    Drop config-mirror and round-trip blind index tests

--- test/models/admin_api_key_test.rb
+++ test/models/admin_api_key_test.rb
@@ -1,13 +1,6 @@
 require "test_helper"
 
 class AdminApiKeyTest < ActiveSupport::TestCase
-  test "uses a fast blind index for high-entropy tokens" do
-    options = AdminApiKey.blind_indexes.fetch(:token)
-
-    assert_equal :pbkdf2_sha256, options[:algorithm]
-    assert_equal({ iterations: 1 }, options[:cost])
-  end
-
   test "stores a fixed-length preview of the token" do
     admin = User.create!(timezone: "UTC", admin_level: :admin)
     key = admin.admin_api_keys.create!(name: "CLI")
@@ -15,16 +8,5 @@ class AdminApiKeyTest < ActiveSupport::TestCase
 
     assert_equal 13, AdminApiKey::TOKEN_PREVIEW_LENGTH
     assert_equal raw_token.first(13), key.reload.token_preview
-  end
-
-  test "finds a key by its virtual token without storing plaintext" do
-    admin = User.create!(timezone: "UTC", admin_level: :admin)
-    key = admin.admin_api_keys.create!(name: "CLI")
-    raw_token = key.token
-
-    key.reload
-
-    assert_nil key.token
-    assert_equal key, AdminApiKey.find_by!(token: raw_token)
   end
 end
